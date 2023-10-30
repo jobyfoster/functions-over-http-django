77ca5a0d6c7c1d1b8f7dabd49c1c9bc295467e53
joby foster <jobyjfoster@gmail.com>
2023-10-30 08:24:55 -0500

    added type annotations

--- app/views.py
+++ app/views.py
@@ -1,14 +1,17 @@
 from django.http.response import HttpResponse
+from django.http.request import HttpRequest
 
 
-def hey_view(request, name):
+def hey_view(request: HttpRequest, name: str) -> HttpResponse:
     return HttpResponse(f"Hey, {name}!")
 
 
-def how_old_view(request, end, birthyear):
+def how_old_view(request: HttpRequest, end: int, birthyear: int) -> HttpResponse:
     return HttpResponse(end - birthyear)
 
 
-def take_order_view(request, burgers, fries, drinks):
+def take_order_view(
+    request: HttpRequest, burgers: int, fries: int, drinks: int
+) -> HttpResponse:
     total = (burgers * 4.5) + (fries * 1.5) + drinks
     return HttpResponse(f"${total:,.2f}")
